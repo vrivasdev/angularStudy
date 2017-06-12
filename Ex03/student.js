ca89962d3a79de20b138dd85931c40ef9cbe7371
vrivas <vrivas@teravisiontech.com>
2017-06-12 11:44:46 -0400

    Ex.03: Students notebook App

---
+++ Ex03/student.js
@@ -0,0 +1,13 @@
+/**
+ * Created by victor on 12/06/17.
+ */
+
+function StudentsController($scope)
+{
+    $scope.students = [
+        {name:"Victor", telephone:"021212311", course:"Computer science"},
+        {name:"Maria", telephone:"04161234123", course:"Maths"},
+        {name:"Pepe", telephone:"04261231231", course:"English"},
+        {name:"Juana", telephone:"04121261271", course:"History"}
+    ];
+}
