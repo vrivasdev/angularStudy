78efbf115d549f3afac9b3565c9a04abcb6d2203
vrivas <victor.rivas.sistemas@gmail.com>
2017-06-13 12:02:58 -0400

    Grunt: Using "shell" plugin to perform some tasks (deleting and moving file to another directory)

--- MyProject/gruntfile.js
+++ MyProject/gruntfile.js
@@ -19,6 +19,18 @@ module.exports = function(grunt)
                 src: 'joined.js', // Once the scripts are joined into one it'll be the source
                 dest: 'build/joined.min.js' // Output "build/joined.js" file compressed
             }
+        },
+        shell:{ /* Task definition: It allows to execute a group of commands. Now the "joined.js" file
+                 * is deleted making a new directory called "deploy" where the "joined.min.js" is going to
+                 * be stored
+                 */
+            multiple:{
+                command:[
+                    'rm joined.js',
+                    'mkdir deploy',
+                    'mv build/joined.min.js deploy/joined.min.js '
+                ].join('&&')
+            }
         }
     });
 
@@ -26,8 +38,9 @@ module.exports = function(grunt)
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-shell');
 
     // Default task loaded when grunt runs
     // Registering the tasks
-    grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+    grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'shell']);
 };
