f2663cebae5e738a7283fb355733836b469df19a
vrivas <victor.rivas.sistemas@gmail.com>
2017-06-21 16:58:26 -0400

    Yeoman project: Listing some values in the main view

--- hello/app/scripts/controllers/main.js
+++ hello/app/scripts/controllers/main.js
@@ -8,10 +8,12 @@
  * Controller of the helloApp
  */
 angular.module('helloApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
+  .controller('MainCtrl', function ( $scope ) {
+    $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
-      'Karma'
+      'Karma',
+      'E2E',
+      'Protractor'
     ];
   });
