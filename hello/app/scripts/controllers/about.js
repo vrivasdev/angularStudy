323f600db04e300ed8f9dd388b3626fb70173389
vrivas <victor.rivas.sistemas@gmail.com>
2017-06-21 15:53:19 -0400

    Yeoman project configuration

---
+++ hello/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name helloApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the helloApp
+ */
+angular.module('helloApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
